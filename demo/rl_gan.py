4c54810a31cdd2e8785ccc937cc7092627f8765a
yinwz <1506015585@qq.com>
2024-01-12 18:31:24 +0800

    1

--- demo/rl_gan.py
+++ demo/rl_gan.py
@@ -30,22 +30,56 @@ class ReplayBuffer:
         return len(self.buffer)
 
 
-# 预训练
-def pretrain(gan, states, outer_iters):
-    labels = torch.ones((states.shape[0], gan.discr_n_outputs))
-    return gan.train(states, labels, outer_iters)
+
+class GoalGAN:
+    """ 将GAN应用在生成目标上"""
+    def __init__(self, state_size, evaluater_size, state_noise_level, *args, **kwargs):
+        self.gan = GAN(gen_n_outputs=state_size, discr_n_outputs=evaluater_size, *args, **kwargs)
+        self.state_size = state_size
+        self.evaluater_size = evaluater_size
+        self.state_noise_level = state_noise_level
+
+    # 预训练
+    def pretrain(self, states, outer_iters):
+        labels = torch.ones((states.shape[0], self.gan.discr_n_outputs))
+        return self.gan.train(states, labels, outer_iters)
+
+    def add_noise_to_states(self, states):
+        noise = np.random.randn
 
 
-def pretrain_uniform(gan, size=1000, report)
 
 
-def generate_initial_goals(env, agent, method="uniform"):
-    if method == "smart_init":
-        pass
-    else:
-        pretrain_uniform(gan)
+def generate_initial_goals(env, agent, horizon=500, size=1e4):
+    current_goal = env.get_current_goal()  # 要求是1维的
+    goals_dim = current_goal.shape[0]
+    goals = np.array([], dtype=np.float32).reshape(-1, goals_dim)  # 创建存goals的数组
+
+    state = env.reset()[0].cuda()
+    done = False
+    steps = 0
+    while goals.shape[0] < size:
+        steps += 1
+        if done or steps >= horizon:
+            steps = 0
+            done = False
+            # 随机新生成一个目标，未完成
+            env.target_sample()
+
+            state = env.reset()[0].cuda()
+        else:
+            action = agent.select_action(state)  # 使用策略
+            next_state, _, done, terminate, info = env.step(action)
+            state = next_state.cuda()
+            if info["good_state"]:
+                goal_from_state = env.state2goal(state)  # 将当前状态变为目标
+                goals = np.append(goals, goal_from_state, axis=0)  # 存数
+
+    return goals
 
 
-def run_train():
-    pass
+def run_train(env, agent, gan):
+    pretrain_goals = generate_initial_goals(env, agent)
+    pretrain(gan, pretrain_goals, 500)
+
 
